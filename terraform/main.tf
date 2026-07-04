85e88bf7f5c1b86a8dd60fdb7bc1209e3f1f92b0
vinaykumarshetkar <vinaykumarshetkar@gmail.com>
2026-07-04 13:04:22 +0530

    Refactor Terraform configuration for Azure resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,170 +1,124 @@
-resource "azurerm_resource_group" "test" {
-  name = "acctestrg"
-  location = "West US 2"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.77.0"
+    }
+  }
 }
 
-resource "azurerm_virtual_network" "test" {
-  name = "acctvn"
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "taskRG" {
+  name = "VM4_RG"
+  location = "Central US"
+}
+
+resource "azurerm_virtual_network" "taskVnet" {
+  name = "Vm4_Vnet"
   address_space = [
     "10.0.0.0/16"]
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  location = azurerm_resource_group.taskRG.location
+  resource_group_name = azurerm_resource_group.taskRG.name
 }
 
 resource "azurerm_subnet" "test" {
-  name = "acctsub"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  virtual_network_name = "${azurerm_virtual_network.test.name}"
-  address_prefix = "10.0.2.0/24"
+  name                 = "VM4_sub"
+  resource_group_name  = azurerm_resource_group.taskRG.name
+  virtual_network_name = azurerm_virtual_network.taskVnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_public_ip" "test" {
-  name = "publicIPForLB"
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  allocation_method       = "Static"
-  idle_timeout_in_minutes = 30
-  domain_name_label="dellemcappdemo"
-  tags {
-    environment = "staging"
+resource "azurerm_public_ip" "taskPIP" {
+  name                = "VM4-PIP"
+  location            = azurerm_resource_group.taskRG.location
+  resource_group_name = azurerm_resource_group.taskRG.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
+resource "azurerm_network_security_group" "taskNSG" {
+  name                = "VM4-NSG"
+  location            = azurerm_resource_group.taskRG.location
+  resource_group_name = azurerm_resource_group.taskRG.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+
+    source_port_range          = "*"
+    destination_port_range     = "22"
+
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
 
-resource "azurerm_network_security_group" "test" {
-  name                = "acceptanceTestSecurityGroup1"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  tags {
-    environment = "staging"
-  }
-}
+resource "azurerm_network_interface" "taskNIC" {
+  name                = "VM4-NIC"
+  location            = azurerm_resource_group.taskRG.location
+  resource_group_name = azurerm_resource_group.taskRG.name
 
-resource "azurerm_network_security_rule" "test1" {
-  name                        = "SSH"
-  priority                    = 340
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = "${azurerm_resource_group.test.name}"
-  network_security_group_name = "${azurerm_network_security_group.test.name}"
-}
-
-resource "azurerm_network_security_rule" "test2" {
-  name                        = "8080"
-  priority                    = 1020
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "8080"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = "${azurerm_resource_group.test.name}"
-  network_security_group_name = "${azurerm_network_security_group.test.name}"
-}
-
-
-
-
-resource "azurerm_network_interface" "test" {
-  count = 1
-  name = "acctni${count.index}"
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  network_security_group_id = "${azurerm_network_security_group.test.id}"
   ip_configuration {
-    name = "testConfiguration"
-    subnet_id = "${azurerm_subnet.test.id}"
-    private_ip_address_allocation = "dynamic"
-    public_ip_address_id = "${azurerm_public_ip.test.id}"
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.test.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.taskPIP.id
   }
 }
 
-resource "azurerm_managed_disk" "test" {
-  count = 1
-  name = "datadisk_existing_${count.index}"
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  storage_account_type = "Standard_LRS"
-  create_option = "Empty"
-  disk_size_gb = "1023"
+resource "azurerm_network_interface_security_group_association" "taskAssoc" {
+  network_interface_id      = azurerm_network_interface.taskNIC.id
+  network_security_group_id = azurerm_network_security_group.taskNSG.id
 }
 
-resource "azurerm_availability_set" "avset" {
-  name = "avset"
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  platform_fault_domain_count = 2
-  platform_update_domain_count = 2
-  managed = true
+data "azurerm_ssh_public_key" "sshkey" {
+  name                = "newKey"
+  resource_group_name = "vinay"
 }
 
-resource "azurerm_virtual_machine" "test" {
-  count = 1
-  name = "dellemc_demo"
-  location = "${azurerm_resource_group.test.location}"
-  availability_set_id = "${azurerm_availability_set.avset.id}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  network_interface_ids = ["${azurerm_network_interface.test.id}"]
-  vm_size = "Standard_DS1_v2"
+resource "azurerm_linux_virtual_machine" "taskVM" {
+  name                = "VM4"
+  resource_group_name = azurerm_resource_group.taskRG.name
+  location            = azurerm_resource_group.taskRG.location
+  size                = "Standard_D2s_v3"
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
+  admin_username = "azureuser"
 
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  network_interface_ids = [
+    azurerm_network_interface.taskNIC.id
+  ]
 
-  storage_image_reference {
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = data.azurerm_ssh_public_key.sshkey.public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
     publisher = "Canonical"
-    offer = "UbuntuServer"
-    sku = "16.04-LTS"
-    version = "latest"
+    offer     = "ubuntu-24_04-lts"
+    sku        = "server"
+    version    = "latest"
   }
 
-  storage_os_disk {
-    name = "myosdisk${count.index}"
-    caching = "ReadWrite"
-    create_option = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  # Optional data disks
-  storage_data_disk {
-    name = "datadisk_new_${count.index}"
-    managed_disk_type = "Standard_LRS"
-    create_option = "Empty"
-    lun = 0
-    disk_size_gb = "1023"
-  }
-
-  storage_data_disk {
-    name = "${element(azurerm_managed_disk.test.*.name, count.index)}"
-    managed_disk_id = "${element(azurerm_managed_disk.test.*.id, count.index)}"
-    create_option = "Attach"
-    lun = 1
-    disk_size_gb = "${element(azurerm_managed_disk.test.*.disk_size_gb, count.index)}"
-  }
-
-  os_profile {
-    computer_name = "dellemcdemovm"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags {
-    environment = "staging"
-  }
+  disable_password_authentication = true
 }
 
 
-output "public_ip_address" {
-  value = "${azurerm_public_ip.test.ip_address}"
+output "public_ip" {
+  value = azurerm_public_ip.taskPIP.ip_address
+}
+
+output "private_ip" {
+  value = azurerm_network_interface.taskNIC.private_ip_address
 }
