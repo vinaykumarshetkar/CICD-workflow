db7dbaa46fb19d99bdf0cce165a20b49bafd3abf
vinaykumarshetkar <vinaykumarshetkar@gmail.com>
2026-07-11 00:35:04 +0530

    Update source image reference for Ubuntu version

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,12 +117,12 @@ resource "azurerm_linux_virtual_machine" "taskVM" {
     storage_account_type = "Standard_LRS"
   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "ubuntu-24_04-lts"
-    sku        = "server"
-    version    = "latest"
-  }
+source_image_reference {
+  publisher = "Canonical"
+  offer      = "0001-com-ubuntu-server-focal"
+  sku        = "20_04-lts-gen2"
+  version    = "latest"
+}
 
   disable_password_authentication = true
 }
